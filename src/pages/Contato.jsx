40e1f5e62a56439a824f9f2a8d365bff1abc3359
Nicholas Belo <nickbelo2201@gmail.com>
2026-09-15 09:04:01 -0300

    feat: criando rotas

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,8 @@
+
+const Contato = () => {
+  return (
+    <h1>Fale conosco</h1>
+  )
+}
+
+export default Contato
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,8 @@
+
+const Sobre = () => {
+  return (
+    <h1>Sobre Nós</h1>
+  )
+}
+
+export default Sobre
